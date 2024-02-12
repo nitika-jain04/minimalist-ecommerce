45426e9a0a94380a9d0781ca25ea92a1e8076a5c
Nitika Jain <nitikaaajain.04@gmail.com>
2024-02-12 19:37:05 +0530

    banner for furniture

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 export default function Banner2() {
   return (
     <div className="px-5 mt-28 md:flex xl:px-36 w-full">
-      <div className="bg-gray-200 flex flex-col gap-3 justify-between px-10 py-20 md:px-32 md:py-32 md:w-1/2">
+      <div className="bg-gray-200 flex flex-col gap-3 justify-between px-10 py-20 md:px-32 md:py-32 md:w-1/2 xl:py-40">
         <h2 className="text-3xl font-bold font-blinker tracking-tight">
-          Creative Harmonious Living
+          Creative & Harmonious Living
         </h2>
         <p className="text-sm font-medium font-blinker leading-6">
           RAOUF Products are all made to standard sizes so that you can mix and
