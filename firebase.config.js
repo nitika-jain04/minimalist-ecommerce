338b98f398b1b33e47eceaceac46bb3c57eb2b3c
Nitika Jain <nitikaaajain.04@gmail.com>
2024-02-23 18:45:49 +0530

    config file

---
+++ firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDXeXgsujn8FHxxbOxqMK5vHUTe_gQ4yeI",
+  authDomain: "ecommerce-otp-123.firebaseapp.com",
+  projectId: "ecommerce-otp-123",
+  storageBucket: "ecommerce-otp-123.appspot.com",
+  messagingSenderId: "477170084161",
+  appId: "1:477170084161:web:4a87a9cd00fb7d27d999c0",
+  measurementId: "G-LBY1NH1QF4",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
