375d027c6f310d853f50c8a03da00b3903e71abb
Nitika Jain <nitikaaajain.04@gmail.com>
2024-02-23 18:49:10 +0530

    alignment fixes

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,22 +1,37 @@
 "use client";
 
 import { useContext } from "react";
+import { useRouter } from "next/navigation";
 import CartContext from "@/store/shopping-cart-context";
 
 import Link from "next/link";
 import { useState, useEffect } from "react";
 import { PiShoppingCartBold } from "react-icons/pi";
 import { RxHamburgerMenu } from "react-icons/rx";
+import { MdLogin, MdLogout } from "react-icons/md";
 import Billing from "./Billing";
 import Backdrop from "./Backdrop";
 import Sidebar from "./Sidebar";
 
 export default function Navbar() {
+  const router = useRouter();
   const cartCtx = useContext(CartContext);
   const [open, setOpen] = useState(false);
   const [openSidebar, setOpenSidebar] = useState(false);
   const [navPadding, setNavPadding] = useState("py-4");
   const [totalCartItems, setTotalCartItems] = useState(0);
+  const [user, setUser] = useState(null);
+
+  function handleClick(click) {
+    if (user === null) {
+      router.push("/account");
+    }
+    setOpen(click);
+  }
+
+  useEffect(() => {
+    setUser(JSON.parse(localStorage.getItem("user")));
+  }, []);
 
   useEffect(() => {
     const newTotalCartItems = cartCtx.items.reduce(
@@ -25,10 +40,6 @@ export default function Navbar() {
     );
     setTotalCartItems(newTotalCartItems);
   }, [cartCtx.items]);
-
-  function handleClick(click) {
-    setOpen(click);
-  }
 
   function handleSidebar(click) {
     setOpenSidebar(click);
@@ -52,7 +63,7 @@ export default function Navbar() {
 
   return (
     <nav
-      className={`flex justify-between px-5 shadow-md mb-5 sticky top-0 z-50 bg-white xl:px-36 overscroll-none overscroll-y-none ${navPadding}`}
+      className={`flex justify-between px-5 shadow-md mb-5 sticky top-0 z-40 bg-white xl:px-36 overscroll-none overscroll-y-none ${navPadding}`}
     >
       <Link href="/">
         <img className="w-1/2" src="/logo-real.png" alt="Logo" />
@@ -76,15 +87,39 @@ export default function Navbar() {
           <sup className="absolute -right-2 -top-2 text-sm px-2 text-white bg-red-600 rounded-full font-blinker font-medium">
             {totalCartItems}
           </sup>
+
           <PiShoppingCartBold
-            className="text-2xl m-2"
+            className={`text-2xl m-2`}
             onClick={() => handleClick(true)}
           />
         </div>
+
         <RxHamburgerMenu
           className="text-xl font-bold sm:hidden"
           onClick={() => handleSidebar(true)}
         />
+
+        <button className="bg-red-500 gap-2 items-center text-white p-3 rounded-xl font-blinker tracking-wide border-2 border-white hover:scale-105 hover:bg-white hover:text-red-600 hover:border-2 hover:border-red-500 transition-all duration-300 ease-in">
+          {user === null ? (
+            <Link href="/account">
+              <div className="flex gap-2 items-center">
+                <MdLogin />
+                <h1>Log In</h1>
+              </div>
+            </Link>
+          ) : (
+            <div
+              className="flex gap-2 items-center"
+              onClick={() => {
+                localStorage.removeItem("user");
+                router.push("/account");
+              }}
+            >
+              <MdLogout />
+              <h1>Sign Out</h1>
+            </div>
+          )}
+        </button>
       </div>
 
       {open && <Backdrop handleClick={handleClick} />}
