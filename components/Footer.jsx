3d00f15ea4fd83c2ab8db90517378bd56e11374a
Nitika Jain <nitikaaajain.04@gmail.com>
2024-02-24 13:21:15 +0530

    google font and css addons

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <div>
       <div className="flex gap-5 flex-col py-24 bg-zinc-900 mt-28">
-        <h2 className="text-center text-white text-3xl font-extrabold">
+        <h2 className="text-center text-white text-3xl font-bold">
           Newsletter
         </h2>
         <div className="flex gap-2 self-center">
@@ -13,35 +13,37 @@ export default function Footer() {
             name="email"
             id=""
             placeholder="your@gmail.com"
-            className="p-2"
+            className="p-2 placeholder:styles.blinker"
           />
-          <button className="bg-white p-1">Subscribe</button>
+          <button className="bg-white p-1 styles.blinker tracking-wide text-lg">
+            Subscribe
+          </button>
         </div>
       </div>
 
       <div className="bg-black text-white flex flex-col justify-center items-center gap-5 py-5">
-        <div className="flex gap-5 flex-wrap p-5 justify-center font-blinker">
-          <a className="hover:cursor-pointer hover:underline" href="/">
+        <div className="flex gap-5 flex-wrap p-5 justify-center styles.blinker">
+          <a className="hover:cursor-pointer hover:underline text-lg" href="/">
             About
           </a>
-          <a className="hover:cursor-pointer hover:underline" href="/">
+          <a className="hover:cursor-pointer hover:underline text-lg" href="/">
             Store Locator
           </a>
-          <a className="hover:cursor-pointer hover:underline" href="/">
+          <a className="hover:cursor-pointer hover:underline text-lg" href="/">
             FAQs
           </a>
-          <a className="hover:cursor-pointer hover:underline" href="/">
+          <a className="hover:cursor-pointer hover:underline text-lg" href="/">
             News
           </a>
-          <a className="hover:cursor-pointer hover:underline" href="/">
+          <a className="hover:cursor-pointer hover:underline text-lg" href="/">
             Careers
           </a>
-          <a className="hover:cursor-pointer hover:underline" href="/">
+          <a className="hover:cursor-pointer hover:underline text-lg" href="/">
             Contact Us
           </a>
         </div>
 
-        <p className="text-center font-blinker">
+        <p className="text-center styles.blinker text-base mb-5">
           Designed by <span className="hover:cursor-pointer">Raouf</span>
         </p>
       </div>
