6e4636e614e8283c6005fd35e1826d88f4d1db50
Nitika Jain <nitikaaajain.04@gmail.com>
2024-02-24 13:14:40 +0530

    google font added

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -16,12 +16,12 @@ export default function Sidebar({ isOpen, handleSidebar }) {
       </div>
       <ul className="flex flex-col gap-5 mt-5">
         <Link href="/categories/all">
-          <li className="bg-gray-200 p-2 font-blinker hover:border-2 hover:border-gray-400 border-2 border-gray-200 transition-all duration-300 ease-in-out">
+          <li className="bg-gray-200 p-2 styles.blinker hover:border-2 hover:border-gray-400 border-2 border-gray-200 transition-all duration-300 ease-in-out">
             CATEGORIES
           </li>
         </Link>
         <Link href="/categories/product/20">
-          <li className="bg-gray-200 p-2 font-blinker hover:border-2 hover:border-gray-400 border-2 border-gray-200 transition-all duration-300 ease-in-out">
+          <li className="bg-gray-200 p-2 styles.blinker hover:border-2 hover:border-gray-400 border-2 border-gray-200 transition-all duration-300 ease-in-out">
             PRODUCT PAGE
           </li>
         </Link>
